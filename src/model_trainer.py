885cb9783980ed6691e63d447ea5bcda77b16601
shravani82 <avalkarshravani@gmail.com>
2024-09-18 13:27:18 +0530

    model trainer file

---
+++ src/model_trainer.py
@@ -0,0 +1,17 @@
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+class ModelTrainer:
+    def _init_(self):
+        self.model = RandomForestRegressor()
+
+    def train_model(self, X_train, y_train):
+        """Trains the RandomForestRegressor model."""
+        self.model.fit(X_train, y_train)
+        print("Model trained successfully.")
+        return self.model
+
+    def save_model(self, model, model_path='model.pkl'):
+        """Saves the trained model to a file."""
+        joblib.dump(model, model_path)
+        print(f"Model saved to {model_path}.")
