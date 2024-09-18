c1ba118f2c16565f1bc93de31117025d0498613a
shravani82 <avalkarshravani@gmail.com>
2024-09-18 13:28:42 +0530

    model evaluator file added

---
+++ src/model_evaluator.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import mean_squared_error
+
+class ModelEvaluator:
+    def _init_(self, model):
+        self.model = model
+
+    def evaluate(self, X_test, y_test):
+        """Evaluates the model's performance."""
+        predictions = self.model.predict(X_test)
+        mse = mean_squared_error(y_test, predictions)
+        print(f"Model Evaluation - Mean Squared Error: {mse:.2f}")
+        return mse
